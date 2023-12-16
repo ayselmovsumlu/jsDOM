3a1d2de36c574db93f6c6450d6691eb5b13b4949
aysel movsumlu <aysel.movsumlu@mail.ru>
2023-12-16 19:37:09 +0000

    add

--- app.js
+++ app.js
@@ -4,8 +4,19 @@ $(document).ready(function () {
     $('.btn').click(function (e) {
         e.preventDefault();
 
+        // let f = a => {
 
+        //     for (let i = 1; i <= a; i++) {
+        //         $('.col-md-12').append(a);
+        //     } 
+        // }
+
+        let d = b => {
+            $('.col-md-12').append(b);
+        }
+        d($('input').val())
 
     });
+
 
 });
