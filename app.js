f772dae2af75054541f26000b5a37def918f0513
aysel movsumlu <aysel.movsumlu@mail.ru>
2023-12-17 06:34:39 +0000

    minibox

--- app.js
+++ app.js
@@ -4,16 +4,16 @@ $(document).ready(function () {
     $('.btn').click(function (e) {
         e.preventDefault();
 
-        // let f = a => {
+        let f = a => {
 
-        //     for (let i = 1; i <= a; i++) {
-        //         $('.col-md-12').append(a);
-        //     } 
-        // }
-
-        let d = b => {
-            $('.col-md-12').append(b);
+            for (let i = 1; i <= a; i++) {
+                $('.answer').append(`<div class="minibox">${a}</div>`);
+            } 
         }
+
+        let d = b => { b*1?  f($('input').val()) : $('.answer').append(`<div class="minibox">${b}</div>`) ;
+        }
+
         d($('input').val())
 
     });
