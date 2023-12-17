6fc64a15f2accf71dcd59446bac90b645ab46449
aysel movsumlu <aysel.movsumlu@mail.ru>
2023-12-17 06:44:38 +0000

    fontsize

--- app.js
+++ app.js
@@ -7,11 +7,11 @@ $(document).ready(function () {
         let f = a => {
 
             for (let i = 1; i <= a; i++) {
-                $('.answer').append(`<div class="minibox">${a}</div>`);
+                $('.answer').append(`<div class="minibox"><h3>${a}</h3></div>`);
             } 
         }
 
-        let d = b => { b*1?  f($('input').val()) : $('.answer').append(`<div class="minibox">${b}</div>`) ;
+        let d = b => { b*1?  f($('input').val()) : $('.answer').append(`<div class="minibox"><h3>${b}</h3></div>`) ;
         }
 
         d($('input').val())
