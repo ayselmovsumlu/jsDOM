01fbbb3b8b5d569624307991fcc95c30925e2033
aysel movsumlu <aysel.movsumlu@mail.ru>
2023-12-18 19:05:42 +0000

    height

--- app.js
+++ app.js
@@ -8,13 +8,17 @@ $(document).ready(function () {
 
             for (let i = 1; i <= a; i++) {
                 $('.answer').append(`<div class="minibox"><h3>${a}</h3></div>`);
-            } 
+
+            }
         }
 
-        let d = b => { b*1?  f($('input').val()) : $('.answer').append(`<div class="minibox"><h3>${b}</h3></div>`) ;
+        let d = b => {
+            b * 1 ? f($('input').val()) : $('.answer').append(`<div class="minibox"><h3>${b}</h3></div>`);
         }
 
-        d($('input').val())
+
+        d($('input').val());
+
 
     });
 
